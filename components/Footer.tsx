6e946bb86fc5cdbf495e24f8b445355c22926382
SaikotHouse <65276816+saikothouse@users.noreply.github.com>
2024-11-04 16:14:52 +0600

    Delete components directory

--- components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="bg-gray-800 text-white text-center p-4 mt-4">
-      <p>&copy; {new Date().getFullYear()} My Blog. All rights reserved.</p>
-    </footer>
-  );
-};
-
-export default Footer;
